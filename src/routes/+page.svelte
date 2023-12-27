34671dcc137e3cf12831bafd1e6e5213ae091e54
ismailhasannnnnn <ismailhasan0132@gmail.com>
2023-12-26 18:33:54 -0800

    Added website files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<style>
+    main{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+</style>
+
+<main>
+    <div>
+        <h1>404</h1>
+        <h2>Page not found</h2>
+        <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+</main>
